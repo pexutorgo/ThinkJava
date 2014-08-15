ef75f38d337b3b5e71f0c91f897be0a5b06fce05
nocturnaltortoise <simonturner34@gmail.com>
2014-08-15 20:48:17 +0100

    First chapter done

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+	//beginning the Think Java book
+	
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		System.out.println("How are you?");
+	}
+
+}
